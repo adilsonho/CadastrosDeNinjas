67fae3f7dfdaf6f5129744bc7a4d0a3644c7fd14
adilsonho <adilsonholandez9@gmail.com>
2025-10-07 17:45:44 -0300

    feat(ninja-mapper): adicionar mapeamento do campo imgUrl no NinjaDTO

--- src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaMapper.java
@@ -12,6 +12,7 @@ public class NinjaMapper {
         ninjaModel.setNome(ninjaDTO.getNome());
         ninjaModel.setEmail(ninjaDTO.getEmail());
         ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
         ninjaModel.setRank(ninjaDTO.getRank());
         ninjaModel.setMissoes(ninjaDTO.getMissoes());
 
@@ -25,6 +26,7 @@ public class NinjaMapper {
         ninjaDTO.setNome(ninjaModel.getNome());
         ninjaDTO.setEmail(ninjaModel.getEmail());
         ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
         ninjaDTO.setMissoes(ninjaModel.getMissoes());
         ninjaDTO.setRank(ninjaModel.getRank());
 
